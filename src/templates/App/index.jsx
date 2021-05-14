a1e20ba223d20fb8c992c7b5d5ce9b41fbb3edf6
Humberto Brasileiro <humbertobtoscano@gmail.com>
2021-05-14 12:32:27 -0300

    first commit

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
